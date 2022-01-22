0baa9feb9bcfb40e68f992ea1b534d2ab08ea3ce
tmartin <xtom2301@gmail.com>
2022-01-22 20:17:52 +0100

    checkTodo, deleteTodo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,55 @@ import Todo from './Todo';
 
 const App = () => {
   const [todos, setTodos] = useState(items);
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {}, []);
+  const countOpenTodos = () => {
+    let doneTodos = todos.filter((todo) => {
+      return !todo.done;
+    });
+    setCount(doneTodos.length);
+  };
+
+  const checkTodo = (i) => {
+    const list = [...todos];
+    if (list[i].done) {
+      list[i].done = false;
+    } else {
+      list[i].done = true;
+    }
+    setTodos(list);
+  };
+
+  const deleteTodo = (i) => {
+    const list = [...todos];
+    list.splice(i, 1);
+    setTodos(list);
+  };
+
+  useEffect(() => {
+    countOpenTodos();
+  }, [todos]);
 
   return (
-    <div className='max-w-2xl m-auto mt-40 bg-zinc-400'>
-      <h1 className='text-white p-8 text-center font-bold text-4xl bg-zinc-900'>
+    <div className='max-w-2xl m-auto mt-40 bg-zinc-900 text-white'>
+      <h1 className='text-white p-8 text-center font-bold text-4xl '>
         Yet Another Todo App
       </h1>
+      <h2 className='text-center mb-4 font-semibold text-xl'>
+        Open Todos: {count}
+      </h2>
       <div>
         {todos.map((todo, i) => {
-          return <Todo key={i} name={todo.name} i={i} done={todo.done} />;
+          return (
+            <Todo
+              key={i}
+              name={todo.name}
+              i={i}
+              done={todo.done}
+              onCheckTodo={checkTodo}
+              onDelete={deleteTodo}
+            />
+          );
         })}
       </div>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-const Todo = ({ name, i, done }) => {
+const Todo = ({ name, i, done, onCheckTodo, onDelete }) => {
   return (
     <div
       className={
@@ -7,8 +7,20 @@ const Todo = ({ name, i, done }) => {
           : 'p-6 flex justify-between items-center border-t-8 border-indigo-100 bg-red-500'
       }
     >
-      <h1 className='text-2xl font-bold text-white'>{name}</h1>
-      <button className='bg-zinc-900 p-4 text-white font-semibold'>
+      <h1
+        className='text-2xl font-bold cursor-pointer'
+        onClick={() => {
+          onCheckTodo(i);
+        }}
+      >
+        {name}
+      </h1>
+      <button
+        className='bg-zinc-900 p-4 font-semibold'
+        onClick={() => {
+          onDelete(i);
+        }}
+      >
         Delete
       </button>
     </div>
